e36770c164e785374cc551bfea9e21e3d3b04156
ethanseow <eseow6088@bths.edu>
2020-02-12 21:56:44 -0500

    debug clean up

--- BasicBluetoothSerialRead/BasicBluetoothSerialRead.ino
+++ BasicBluetoothSerialRead/BasicBluetoothSerialRead.ino
@@ -45,88 +45,48 @@ void setup() {
 void loop() {
   if (Serial.available() > 0) {
     state = Serial.read();
-    //    Serial.println(state);
     receivedData = true;
   }
   if (state == 48) {
     // forward
     leftSetSpeed = ZERO + SPEED;
     rightSetSpeed = ZERO - SPEED;
-    //    Serial.print("LEFT:");
-    //    Serial.println(leftSetSpeed);
-    //
-    //    Serial.print("RIGHT:");
-    //    Serial.println(rightSetSpeed);
   }
   else if (state == 49) {
     // backward
     leftSetSpeed = ZERO - SPEED;
     rightSetSpeed = ZERO + SPEED;
-    //    Serial.print("LEFT:");
-    //    Serial.println(leftSetSpeed);
-    //
-    //    Serial.print("RIGHT:");
-    //    Serial.println(rightSetSpeed);
   }
   else if (state == 50) {
     // left
 
     leftSetSpeed = ZERO + SPEED;
     rightSetSpeed = ZERO + SPEED;
-
-    //    Serial.print("LEFT:");
-    //    Serial.println(leftSetSpeed);
-    //
-    //    Serial.print("RIGHT:");
-    //    Serial.println(rightSetSpeed);
   }
   else if (state == 51) {
     // right
 
     leftSetSpeed = ZERO - SPEED;
     rightSetSpeed = ZERO - SPEED;
-
-    //    Serial.print("LEFT:");
-    //    Serial.println(leftSetSpeed);
-    //
-    //    Serial.print("RIGHT:");
-    //    Serial.println(rightSetSpeed);
   }
   else if (state == 52) {
     // stop
     leftSetSpeed = ZERO;
     rightSetSpeed = ZERO;
-
-    //    Serial.print("LEFT:");
-    //    Serial.println(leftSetSpeed);
-    //
-    //    Serial.print("RIGHT:");
-    //    Serial.println(rightSetSpeed);
   }
   if (receivedData) {
     receivedData = false;
-    //    Serial.print("LEFT Motor:");
-    //    Serial.println(leftSide.read());
-    //
-    //    Serial.print("RIGHT Motor:");
-    //    Serial.println(rightSide.read());
   }
   if (leftCurrentSpeed > leftSetSpeed) {
     leftCurrentSpeed = leftCurrentSpeed - acceleration;
   } else if (leftCurrentSpeed < leftSetSpeed) {
     leftCurrentSpeed = leftCurrentSpeed + accelerationadd .;
-  } else if (leftCurrentSpeed == leftSetSpeed) {
-    //    Serial.print("On Setpoint Set");
-    //    Serial.println(leftCurrentSpeed);
   }
 
   if (rightCurrentSpeed > rightSetSpeed) {
     rightCurrentSpeed = rightCurrentSpeed - acceleration;
   } else if (rightCurrentSpeed < rightSetSpeed) {
     rightCurrentSpeed = rightCurrentSpeed + acceleration;
-  } else if (rightCurrentSpeed == rightSetSpeed) {
-    //    Serial.print("On Setpoint Right");
-    //    Serial.println(rightCurrentSpeed);
   }
   leftSide.write(leftCurrentSpeed);
   rightSide.write(rightCurrentSpeed);
